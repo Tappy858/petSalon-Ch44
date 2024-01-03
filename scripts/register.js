ace4347c6c850d040277fb172636568f96ec555a
darius <darius_taplin@yahoo.com>
2024-01-02 19:21:57 -0800

    class2

--- scripts/register.js
+++ scripts/register.js
@@ -11,58 +11,77 @@ let petSalon = {
         open:"8:00 am",
         closed:"4:00 pm"
     },
-    pets:[
-        {
-            Name:"Lucas",
-            Age:80,
-            Gender:"Male",
-            Service:"grooming",
-            Breed:"Akita"
-        },
-        {
-            Name:"John",
-            Age:60,
-            Gender:"Male",
-            Service:"nails",
-            Breed:"Rottweiler"
-        },
-        {
-            Name:"Chanel",
-            Age:50,
-            Gender:"Female",
-            Service:"grooming",
-            Breed:"Barbet"
-        }
-        
-    ]
+    pets:[]
 }
+
+//object constructor (function)
+function Pet(name,age,gender,breed){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+}
+
+function register(){
+    console.log("Registering");
+    //get the values from the input
+    let inputName = document.getElementById("txtName").value;
+    let inputAge = document.getElementById("txtAge").value;
+    let inputGender = document.getElementById("txtGender").value;
+    let inputBreed = document.getElementById("txtBreed").value;
+
+    console.log(inputAge,inputGender,inputName,inputBreed);
+
+    //creating the obj
+    let newPet = new Pet(inputAge,inputGender,inputName);
+
+    //push the object
+    petSalon.pets.push(newPet);
+
+    //display the pets array on the console
+    console.log(petSalon.pets);
+
+}
+
+//creating pets using constructor
+let p1 = new Pet("scooby",60,"male");
+let p2 = new Pet("yolo",50,"male");
+let p3 = new Pet("boo",40,"male");
+//pushing pets into array
+petSalon.pets.push(p1,p2,p3);
+
+console.log(petSalon.pets);
+//console.log(p1,p2,p3);
+
+
+
 //display registered pets count
-function displayPetCount() {
-    const petCount = document.getElementById("petsCount");
-    petCount.textContent = `Total Pets Registered: ${petSalon.pets.length}`;
-}
-displayPetCount();
+//function displayPetCount() {
+//     const petCount = document.getElementById("petsCount");
+//     petCount.textContent = `Total Pets Registered: ${petSalon.pets.length}`;
+// }
+// displayPetCount();
 
-//displaying names
-function displayPetNames() {
-    const petNames = document.getElementById('petsNames');
-    petNames.innerHTML = ''; // Clear existing list
+// //displaying names
+// function displayPetNames() {
+//     const petNames = document.getElementById('petsNames');
+//     petNames.innerHTML = ''; // Clear existing list
 
-    // Loop through the pets array and add each pet name to the list
-    for (let i = 0; i < petSalon.pets.length; i++) {
-        const listPets = document.createElement('li');
-        listPets.textContent = petSalon.pets[i].Name;
-        petNames.appendChild(listPets);
-    }
-}
-displayPetNames();
+//     // Loop through the pets array and add each pet name to the list
+//     for (let i = 0; i < petSalon.pets.length; i++) {
+//         const listPets = document.createElement('li');
+//         listPets.textContent = petSalon.pets[i].Name;
+//         petNames.appendChild(listPets);
+//     }
+// }
+// displayPetNames();
 
 
-console.table(petSalon.pets);
+// console.table(petSalon.pets);
 
-console.log(petSalon.pets[0].name);
-console.log(petSalon.pets[1].name);
-console.log(petSalon.pets[2].name);
+// console.log(petSalon.pets[0].name);
+// console.log(petSalon.pets[1].name);
+// console.log(petSalon.pets[2].name);
 
 //total of elements in an array
 //console.log(petSalon.pets.length);
